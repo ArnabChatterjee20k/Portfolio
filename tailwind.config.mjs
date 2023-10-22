95f449202c27cf1bdc3cdbf842d4ef0d9206fdc5
arnab <arnabchatterjee.ac.2@gmail.com>
2023-10-22 14:16:33 +0530

    feat -- generated the tailwind the custom classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundColor: ({ theme }) => ({
+        primary: {
+          light: theme("colors.gray.100"),
+          dark: theme("colors.slate.950"),
+        },
+        secondary: {
+          light: "white",
+          dark: theme("colors.slate.900"),
+        },
+      }),
+      textColor: ({ theme }) => ({
+        primary: {
+          light: "black",
+          dark: theme("colors.gray.200"),
+        },
+        secondary: {
+          light: "white",
+          dark: theme("colors.gray.300"),
+        },
+        link: theme("colors.blue.600"),
+      }),
+    },
+  },
+  plugins: [],
+};
