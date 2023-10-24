87c9ce691e14ce56b4b5d564f8dc2754a250a682
arnab <arnabchatterjee.ac.2@gmail.com>
2023-10-24 22:55:05 +0530

    feat -- Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,13 +1,27 @@
-import type React from "react";
+import React from "react";
 import { twMerge } from "tailwind-merge";
+import Vectorarrow from "../icons/Vectorarrow";
 interface LinkProps extends React.ComponentPropsWithoutRef<"a"> {
   text: string;
+  svgClassName?: React.HTMLProps<"svg">["className"];
 }
-export default function Link({ className, text, href, children }: LinkProps) {
+
+export default function Link({
+  className,
+  text,
+  href,
+  svgClassName,
+}: LinkProps) {
   return (
-    <a href={href} className={twMerge("text-base text-slate-700", className)}>
+    <a
+      href={href}
+      className={twMerge(
+        "text-xl text-slate-700 flex items-center gap-3 cursor-pointer hover:text-black hover:font-medium",
+        className
+      )}
+    >
       {text}
-      {children}
+      <Vectorarrow className={twMerge("h-4 w-5 fill-black", svgClassName)} />
     </a>
   );
 }
