05f4c5a2543599f06cee2cb24646b9649bbc0a30
arnab <arnabchatterjee.ac.2@gmail.com>
2023-10-24 17:58:18 +0530

    style -- fix some styles

--- src/components/NavContainer.tsx
+++ src/components/NavContainer.tsx
@@ -3,17 +3,17 @@ interface Link {
   link: string;
 }
 
+import type React from "react";
 interface NavbarProps {
   children?: React.ReactNode;
 }
 
-import type React from "react";
 import ThemeSwitchButton from "./ThemeSwitchButton";
 // import LinkText from "../components/LinkText.astro"
 
 export default function Navbar({ children }: NavbarProps) {
   return (
-    <nav className="flex max-w-[950px] m-auto py-10 items-center justify-between">
+    <nav className="hidden md:flex max-w-[950px] m-auto py-10 px-5 items-center justify-between">
       {children} 
       <div className="w-6 h-6 bg-black rounded-sm" />
     </nav>
