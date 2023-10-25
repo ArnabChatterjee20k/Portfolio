c955346fe106ce65d92d510a3e94736f981b5029
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2023-10-25 15:33:00 +0530

    style -- fixed navbar

--- src/components/ThemeSwitchButton.tsx
+++ src/components/ThemeSwitchButton.tsx
@@ -1,8 +1,8 @@
-import Icon from "astro-icon"
+import Moon from "../icons/Moon"
 export default function ThemeSwitchButton() {
   return (
-    <div>
-        <Icon pack="" name="moonLinear"/>
-    </div>
+    <button className="bg-gray-300 hover:bg-slate-200 rounded flex items-center justify-center p-2 shadow-2xl">
+      <Moon className="w-10 h-10 md:w-5 md:h-5 fill-black"/>
+    </button>
   )
 }
