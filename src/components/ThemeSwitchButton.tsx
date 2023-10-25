7eef8c61173eb5b248bfcb7a2acf643fb09d5328
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2023-10-25 16:14:45 +0530

    style -- fix navbar

--- src/components/ThemeSwitchButton.tsx
+++ src/components/ThemeSwitchButton.tsx
@@ -1,7 +1,7 @@
 import Moon from "../icons/Moon"
 export default function ThemeSwitchButton() {
   return (
-    <button className="bg-gray-300 hover:bg-slate-200 rounded flex items-center justify-center p-2 shadow-2xl">
+    <button className="hover:bg-gray-300 bg-slate-200 rounded flex items-center justify-center p-2 shadow-2xl">
       <Moon className="w-10 h-10 md:w-5 md:h-5 fill-black"/>
     </button>
   )
