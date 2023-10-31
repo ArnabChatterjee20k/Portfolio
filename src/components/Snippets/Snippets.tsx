cd99e6d13023024ae6b06638f1df18039e1798ce
arnab <arnabchatterjee.ac.2@gmail.com>
2023-10-31 13:07:32 +0530

    feat -- added react as a micro frontend

--- src/components/Snippets/Snippets.tsx
+++ src/components/Snippets/Snippets.tsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+  useParams,
+} from "react-router-dom";
 export default function Snippets() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/snippet/" Component={() => <Link to={"/snippet/hello"}>Hello2</Link>} />
-        <Route path="/snippet/hello" Component={() => <Link to={"/snippet/"}>Hello1</Link>} />
+        <Route path="/snippet/:id" Component={() => <Param />} />
+        <Route path="/snippet/" Component={() => <Dynamic />} />
       </Routes>
     </BrowserRouter>
+  );
+}
+
+function Param() {
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>{id}</h1>
+      <Dynamic/>
+    </div>
+  );
+}
+
+function Dynamic() {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <input
+        type="number"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <Link to={"/snippet/" + text}>Go to {text}</Link>
+    </div>
   );
 }
