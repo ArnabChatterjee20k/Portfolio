38f136c66678ac09da6a8ce54bcde247391d4e80
arnab <arnabchatterjee.ac.2@gmail.com>
2023-11-09 20:26:23 +0530

    feat -- added sessionStorage for caching

--- src/components/Snippets/hooks/useSnippets.tsx
+++ src/components/Snippets/hooks/useSnippets.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import config from "../config";
 import type CodeSnippetInterface from "../types/CodeSnippetInterface";
-import type FETCH_STATUS from "../types/FetchStatus"
+import type FETCH_STATUS from "../types/FetchStatus";
 
 async function fetchSnippets() {
   const url = config.SNIPPET_API_URL;
@@ -10,15 +10,20 @@ async function fetchSnippets() {
   return data;
 }
 
-export default function useSnippets():[FETCH_STATUS,CodeSnippetInterface[]] {
+export default function useSnippets(): [FETCH_STATUS, CodeSnippetInterface[]] {
   const [status, setStatus] = useState<FETCH_STATUS>("idle");
-  const [snippets, setSnippets] = useState<CodeSnippetInterface[]>([]);
+  const [snippets, setSnippets] = useState<CodeSnippetInterface[]>(() => {
+    const data = sessionStorage.getItem("portfolio_data");
+    if (data) return JSON.parse(data);
+    return [];
+  });
   useEffect(() => {
-    if (status === "idle" || status === "error") {
+    if (snippets.length === 0 || status === "error") {
       setStatus("loading");
       fetchSnippets()
         .then((data) => {
           setStatus("success");
+          sessionStorage.setItem("portfolio_data", JSON.stringify(data));
           setSnippets(data);
           data["data"] = data;
         })
